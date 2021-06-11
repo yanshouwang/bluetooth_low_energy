5258f309bffd92cf54215fd2f2da02631ce50fec
闫守旺 <yanshouwang@outlook.com>
2021-06-11 15:15:42 +0800

    CentralManager 开发 & 示例项目开发

---
+++ example/lib/views/home_view.dart
@@ -0,0 +1,59 @@
+import 'package:bluetooth_low_energy/bluetooth_low_energy.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final CentralManager manager;
+  final Map<MAC, Discovery> discoveries;
+
+  _HomeViewState()
+      : manager = CentralManager(),
+        discoveries = {};
+
+  @override
+  void initState() {
+    super.initState();
+    manager.startDiscovery();
+  }
+
+  @override
+  void dispose() {
+    manager.stopDiscovery();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: manager.discovered,
+        builder: (context, AsyncSnapshot<Discovery> snapshot) {
+          if (snapshot.hasData) {
+            final data = snapshot.data!;
+            discoveries[data.address] = data;
+          }
+          return ListView.builder(
+            itemCount: discoveries.length,
+            itemBuilder: (context, i) {
+              final discovery = discoveries[i]!;
+              return Card(
+                child: Column(
+                  children: [
+                    Text(discovery.address.toString()),
+                    Text(discovery.rssi.toString()),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/bluetooth_low_energy.dart
+++ lib/bluetooth_low_energy.dart
@@ -1,6 +1,7 @@
 export 'src/bluetooth_manager.dart';
 export 'src/bluetooth.dart';
 export 'src/discovery.dart';
+export 'src/mac.dart';
 export 'src/uuid.dart';
 export 'src/gatt_service.dart';
 export 'src/gatt_characteristic.dart';
