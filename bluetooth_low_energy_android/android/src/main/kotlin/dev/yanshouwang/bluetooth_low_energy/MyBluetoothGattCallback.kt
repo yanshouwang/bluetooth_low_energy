3eadd9c79f4613eed55f2081278d6cbf4149f42e
yanshouwang <yanshouwang@outlook.com>
2023-08-14 18:17:49 +0800

    fix: 解决未在主线程执行回调的问题

--- bluetooth_low_energy_android/android/src/main/kotlin/dev/yanshouwang/bluetooth_low_energy/MyBluetoothGattCallback.kt
+++ bluetooth_low_energy_android/android/src/main/kotlin/dev/yanshouwang/bluetooth_low_energy/MyBluetoothGattCallback.kt
@@ -9,22 +9,30 @@ import java.util.concurrent.Executor
 class MyBluetoothGattCallback(private val myCentralController: MyCentralController, private val executor: Executor) : BluetoothGattCallback() {
     override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
         super.onConnectionStateChange(gatt, status, newState)
-        myCentralController.onConnectionStateChange(gatt, status, newState)
+        executor.execute {
+            myCentralController.onConnectionStateChange(gatt, status, newState)
+        }
     }
 
     override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
         super.onServicesDiscovered(gatt, status)
-        myCentralController.onServicesDiscovered(gatt, status)
+        executor.execute {
+            myCentralController.onServicesDiscovered(gatt, status)
+        }
     }
 
     override fun onCharacteristicRead(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int) {
         super.onCharacteristicRead(gatt, characteristic, status)
-        myCentralController.onCharacteristicRead(characteristic, status)
+        executor.execute {
+            myCentralController.onCharacteristicRead(characteristic, status)
+        }
     }
 
     override fun onCharacteristicWrite(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int) {
         super.onCharacteristicWrite(gatt, characteristic, status)
-        myCentralController.onCharacteristicWrite(characteristic, status)
+        executor.execute {
+            myCentralController.onCharacteristicWrite(characteristic, status)
+        }
     }
 
     override fun onCharacteristicChanged(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic) {
@@ -35,11 +43,15 @@ class MyBluetoothGattCallback(private val myCentralController: MyCentralControll
 
     override fun onDescriptorRead(gatt: BluetoothGatt, descriptor: BluetoothGattDescriptor, status: Int) {
         super.onDescriptorRead(gatt, descriptor, status)
-        myCentralController.onDescriptorRead(descriptor, status)
+        executor.execute {
+            myCentralController.onDescriptorRead(descriptor, status)
+        }
     }
 
     override fun onDescriptorWrite(gatt: BluetoothGatt, descriptor: BluetoothGattDescriptor, status: Int) {
         super.onDescriptorWrite(gatt, descriptor, status)
-        myCentralController.onDescriptorWrite(descriptor, status)
+        executor.execute {
+            myCentralController.onDescriptorWrite(descriptor, status)
+        }
     }
 }
