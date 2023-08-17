a8065582c1d71730a34f147352db7286f7692ff7
yanshouwang <yanshouwang@outlook.com>
2023-08-17 14:36:33 +0800

    feat: 实现 macOS 接口

---
+++ bluetooth_low_energy_macos/lib/src/my_gatt_descriptor.dart
@@ -0,0 +1,22 @@
+import 'package:bluetooth_low_energy_platform_interface/bluetooth_low_energy_platform_interface.dart';
+
+import 'my_api.g.dart';
+import 'my_gatt_characteristic.dart';
+import 'my_object.dart';
+
+class MyGattDescriptor extends MyObject implements GattDescriptor {
+  final MyGattCharacteristic myCharacteristic;
+  @override
+  final UUID uuid;
+
+  MyGattDescriptor(super.hashCode, this.myCharacteristic, this.uuid);
+
+  factory MyGattDescriptor.fromMyArgs(
+    MyGattCharacteristic myCharacteristic,
+    MyGattDescriptorArgs myArgs,
+  ) {
+    final hashCode = myArgs.key;
+    final uuid = UUID.fromString(myArgs.uuid);
+    return MyGattDescriptor(hashCode, myCharacteristic, uuid);
+  }
+}
--- bluetooth_low_energy_macos/macos/Classes/BluetoothLowEnergymacOS.swift
+++ bluetooth_low_energy_macos/macos/Classes/BluetoothLowEnergymacOS.swift
@@ -3,17 +3,8 @@ import FlutterMacOS
 
 public class BluetoothLowEnergymacOS: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "bluetooth_low_energy_macos", binaryMessenger: registrar.messenger)
-    let instance = BluetoothLowEnergymacOS()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-    default:
-      result(FlutterMethodNotImplemented)
-    }
+        let binaryMessenger = registrar.messenger()
+        let centralController = MyCentralController(binaryMessenger)
+        MyCentralControllerHostApiSetup.setUp(binaryMessenger: binaryMessenger, api: centralController)
   }
 }
