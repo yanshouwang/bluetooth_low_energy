ca46dc40909792ac22f0993987ee4df5d134ef30
yanshouwang <yanshouwang@hebei.dev>
2025-03-25 18:07:56 +0800

    GATT

--- bluetooth_low_energy_android/android/src/main/kotlin/dev/hebei/bluetooth_low_energy_android/GATTCharacteristicWriteType.kt
+++
@@ -1,5 +0,0 @@
-package dev.hebei.bluetooth_low_energy_android
-
-enum class GATTCharacteristicWriteType {
-    WITH_RESPONSE, WITHOUT_RESPONSE,
-}
--- bluetooth_low_energy_android/android/src/main/kotlin/dev/hebei/bluetooth_low_energy_android/GATTPermission.kt
+++
@@ -1,5 +0,0 @@
-package dev.hebei.bluetooth_low_energy_android
-
-enum class GATTPermission {
-    READ, READ_ENCRYPTED, WRITE, WRITE_ENCRYPTED,
-}
